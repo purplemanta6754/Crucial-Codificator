92d93e19deb3afec2a79a6736d461108e32c2300
purplemanta6754 <purplemanta6754@users.noreply.github.com>
2023-08-28 06:54:01 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hola mundo!')
+// Obtener referencia a los botones
+var myButtons = document.querySelectorAll('.btn');
+
+// Función para quitar las clases de animación después de 2 segundos
+function quitarClases(element) {
+  element.classList.remove('animate__animated', 'animate__fadeIn');
+}
+
+// Agregar eventos de clic a los botones
+myButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Agregar las clases de animación al botón
+    button.classList.add('animate__animated', 'animate__fadeIn');
+    
+    // Después de 3 segundos, quitar las clases de animación
+    setTimeout(function() {
+      quitarClases(button);
+    }, 3000);
+  });
+});
+
+// Después de 2 segundos, quitar las clases de animación (si los botones aún las tienen)
+setTimeout(function() {
+  myButtons.forEach(function(button) {
+    quitarClases(button);
+  });
+}, 2000);
