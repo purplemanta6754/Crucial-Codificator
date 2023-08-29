82d21183c50df904fd0439159ffdb4093af67a53
purplemanta6754 <purplemanta6754@users.noreply.github.com>
2023-08-29 06:23:42 -0400

    Código nuevo para la navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,31 @@ setTimeout(function() {
     quitarClases(button);
   });
 }, 2000);
+
+// Obtener referencia a las anclas y botones de la barra de navegación
+var myAnchors = document.querySelectorAll('.navbar-btn');
+
+// Función para quitar las clases de animación después de 2 segundos
+function quitarClases(element) {
+  element.classList.remove('animate__animated', 'animate__fadeIn');
+}
+
+// Agregar eventos de clic a los botones
+myAnchors.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Agregar las clases de animación al botón
+    button.classList.add('animate__animated', 'animate__fadeIn');
+    
+    // Después de 3 segundos, quitar las clases de animación
+    setTimeout(function() {
+      quitarClases(button);
+    }, 3000);
+  });
+});
+
+// Después de 2 segundos, quitar las clases de animación (si los botones aún las tienen)
+setTimeout(function() {
+  myAnchors.forEach(function(button) {
+    quitarClases(button);
+  });
+}, 2000);
