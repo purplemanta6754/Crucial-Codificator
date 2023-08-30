68dfeee877ebe3df8136f07ef4cbf79fdef292dd
purplemanta6754 <purplemanta6754@users.noreply.github.com>
2023-08-30 03:51:35 -0400

    El código ahora es funcional

--- assets/js/virtual_telegraph.js
+++ assets/js/virtual_telegraph.js
@@ -1,9 +1,8 @@
-const key = document.getElementById('key');
+const keyInput = document.getElementById('key');
 const messageInput = document.getElementById('message');
 const playButton = document.getElementById('playButton');
 const dotButton = document.getElementById('dotButton');
 const dashButton = document.getElementById('dashButton');
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 let currentSymbol = '.'; // Por defecto, iniciamos con un punto
 
@@ -24,37 +23,79 @@ playButton.addEventListener('click', () => {
     playMorseCodeSound(morseCode);
 });
 
-messageInput.addEventListener('input', () => {
-    const morseCode = messageInput.value;
-    playMorseCodeSound(morseCode);
+// Agregar un evento 'change' al elemento de entrada de clave
+keyInput.addEventListener('change', () => {
+    updateKey();
 });
 
-function playTelegraphSound(symbol) {
-    const oscillator = audioContext.createOscillator();
-    oscillator.type = 'sine';
-   
-    if (symbol === '.') {
-        oscillator.frequency.setValueAtTime(600, audioContext.currentTime);
-    } else if (symbol === '-') {
-        oscillator.frequency.setValueAtTime(600, audioContext.currentTime);
-    }
-   
-    oscillator.connect(audioContext.destination);
-   
-    oscillator.start();
-    oscillator.stop(audioContext.currentTime + 0.1);
+// Función para actualizar la clave y restablecer el mensaje y símbolo actual
+function updateKey() {
+    const newKey = keyInput.value;
+    currentSymbol = '.';
+    messageInput.value = '';
+    playTelegraphSound(currentSymbol);
 }
 
+// Restaurar la clave y el mensaje al cargar la página
+window.addEventListener('load', () => {
+    updateKey();
+});
+
+// Restaurar la clave y el mensaje al hacer clic en el botón 'Restablecer'
+resetButton.addEventListener('click', () => {
+    updateKey();
+});
+
+// Función para reproducir sonidos pregrabados de puntos y rayas
+function playTelegraphSound(symbol) {
+    const soundPath = symbol === '.' ? '../assets/snds/soundDot.mp3' : '../assets/snds/soundDash.mp3';
+    const sound = new Audio(soundPath);
+    sound.play();
+}
+
+// Función para reproducir sonidos pregrabados de código Morse
 function playMorseCodeSound(code) {
     const timeUnit = 600; // Tiempo en milisegundos para una unidad de tiempo básica
-   
-    code.split('').forEach((char, index) => {
-        setTimeout(() => {
-            if (char === '.') {
-                playTelegraphSound('.');
-            } else if (char === '-') {
-                playTelegraphSound('-');
-            }
-        }, index * timeUnit);
+    const dashDuration = 1200; // Duración en milisegundos del sonido del guion
+    const silenceDuration = 4; // Duración en milisegundos del silencio entre sonidos
+
+    const morseSounds = [];
+
+    code.split('').forEach((char) => {
+        if (char === '.') {
+            morseSounds.push({ symbol: '.', duration: timeUnit });
+        } else if (char === '-') {
+            morseSounds.push({ symbol: '-', duration: dashDuration });
+        }
     });
+
+    // Función recursiva para reproducir los sonidos con el silencio entre ellos
+    function playNextSound(index) {
+        if (index < morseSounds.length) {
+            const sound = morseSounds[index];
+            playTelegraphSound(sound.symbol);
+
+            setTimeout(() => {
+                playNextSound(index + 1);
+            }, sound.duration + silenceDuration);
+        }
+    }
+
+    playNextSound(0);
 }
+
+let currentOscillator = null;
+
+function stopSound() {
+    if (currentOscillator) {
+        currentOscillator.stop();
+        currentOscillator.disconnect();
+        currentOscillator = null;
+    }
+}
+
+const stopButton = document.getElementById('stopButton');
+
+stopButton.addEventListener('click', () => {
+    stopSound();
+});
