9f5a6fa4574f52ae58a247eb354a33c21499bff0
purplemanta6754 <purplemanta6754@users.noreply.github.com>
2023-08-30 06:59:28 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,37 @@
 // Función para abrir telegrafo virtual
 function abrirTelegrafo() {
-  // Obtener el ancho y alto de la pantalla
-  const screenWidth = window.screen.width;
-  const screenHeight = window.screen.height;
+    // Obtener el ancho y alto de la pantalla
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
 
-  // Definir las dimensiones de la ventana emergente
-  const windowWidth = 1400;
-  const windowHeight = 700;
+    // Definir las dimensiones de la ventana emergente
+    const windowWidth = 1400;
+    const windowHeight = 700;
 
-  // Calcular las coordenadas x e y para centrar la ventana emergente en la pantalla
-  const windowX = (screenWidth - windowWidth) / 2;
-  const windowY = (screenHeight - windowHeight) / 2;
+    // Calcular las coordenadas x e y para centrar la ventana emergente en la pantalla
+    const windowX = (screenWidth - windowWidth) / 2;
+    const windowY = (screenHeight - windowHeight) / 2;
 
-  // Abrir la ventana emergente y centrarla en la pantalla
-  window.open("telegrafo-virtual/index.html", "miVentana", `width=${windowWidth}, height=${windowHeight}, top=${windowY}, left=${windowX}`);
+    // Abrir la ventana emergente y centrarla en la pantalla
+    window.open("telegrafo-virtual/index.html", "miVentana", `width=${windowWidth}, height=${windowHeight}, top=${windowY}, left=${windowX}`);
 
-  console.log ('Se ha abierto una nueva ventana para mostrar el telegrafo virtual')
+    console.log('Se ha abierto una nueva ventana para mostrar el telegrafo virtual')
 }
 
-// Función para abrir ventana de personalización
-function establecerTema() {
-  // Obtener el ancho y alto de la pantalla
-  const screenWidth = window.screen.width;
-  const screenHeight = window.screen.height;
+const openModalLink = document.getElementById('openModalLink');
 
-  // Definir las dimensiones de la ventana emergente
-  const windowWidth = 900;
-  const windowHeight = 500;
+openModalLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+});
 
-  // Calcular las coordenadas x e y para centrar la ventana emergente en la pantalla
-  const windowX = (screenWidth - windowWidth) / 2;
-  const windowY = (screenHeight - windowHeight) / 2;
-
-  // Abrir la ventana emergente y centrarla en la pantalla
-  window.open("establecer-tema/index.html", "miVentana", `width=${windowWidth}, height=${windowHeight}, top=${windowY}, left=${windowX}`);
-
-  console.log ('Se ha abierto una nueva ventana para mostrar el telegrafo virtual')
+function closeModal() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    location.reload()
 }
+
+closeModalButton.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
